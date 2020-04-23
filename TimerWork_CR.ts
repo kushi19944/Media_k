37aae445221de514baed76770ede087ed74229c5
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2020-04-23 10:20:05 +0900

    Update TimerWork_CR.ts

--- TimerWork_CR.ts
+++ TimerWork_CR.ts
@@ -219,7 +219,7 @@ async function PageMoveing(SheetData, SheetWorkingRow, PageStatus) {
       RPA.WebBrowser.Until.elementLocated({
         css: '#listTableCreative > tbody > tr:nth-child(1) > td:nth-child(3)'
       }),
-      180000
+      300000
     );
     await RPA.Logger.info('ID出現しました');
   } catch {
@@ -238,7 +238,7 @@ async function PageMoveing(SheetData, SheetWorkingRow, PageStatus) {
       RPA.WebBrowser.Until.elementLocated({
         css: '#listTableCreative > tbody > tr:nth-child(1) > td:nth-child(3)'
       }),
-      180000
+      300000
     );
   } catch {
     PageStatus[0] = 'bad';
@@ -258,7 +258,7 @@ async function StatusChange(SheetData, SheetWorkingRow) {
         RPA.WebBrowser.Until.elementLocated({
           css: `#listTableCreative > tbody > tr:nth-child(${NewNumber}) > td:nth-child(3)`
         }),
-        180000
+        300000
       );
       const IDText = await ID.getText();
       if (IDText == SheetData[2]) {
